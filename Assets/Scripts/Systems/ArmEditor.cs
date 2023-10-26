f2b7655d8fd9ae6ae5462f05b73f9e701cb79bb8
DraiRuben <r.drai@lahorde.school>
2023-10-26 15:23:27 +0200

    arm fix

--- Assets/Scripts/Systems/ArmEditor.cs
+++ Assets/Scripts/Systems/ArmEditor.cs
@@ -8,30 +8,20 @@ using UnityEngine.Tilemaps;
 public class ArmEditor : Draggable
 {
     [SerializeField] private Draggable m_armBase;
+    [SerializeField] private SpriteRenderer m_smallArm;
     [SerializeField] private GameObject m_mediumArm;
     [SerializeField] private GameObject m_longArm; //of the law 
     private int m_armLength = 1;
 
     private void SetArmLength(int value)
     {
-        m_armLength = value;
+        m_armLength = Mathf.Clamp(value,1,3);
         //lengthen or shorten arm
-        if (m_armLength == 1)
-        {
-            m_mediumArm.SetActive(false);
-            m_longArm.SetActive(false);
-            
-        }
-        else if (m_armLength == 2)
-        {
-            m_mediumArm.SetActive(true);
-            m_longArm.SetActive(false);
-        }
-        else if(m_armLength == 3)
-        {
-            m_mediumArm.SetActive(false);
-            m_longArm.SetActive(true);
-        }
+        m_smallArm .enabled = m_armLength == 1; //doesn't disable gameobject since this the draggable should still be usable
+        m_mediumArm.SetActive(m_armLength == 2);
+        m_longArm  .SetActive(m_armLength == 3);
+
+        
     }
     private void OnDisable()
     {
@@ -63,7 +53,7 @@ public class ArmEditor : Draggable
         Tilemap validZones = MapManager.instance.m_armZonesMap;
         while (s_isSomethingDragging)
         {
-            Vector3Int tilePos = validZones.WorldToCell(Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()));
+            Vector3Int tilePos = validZones.WorldToCell((Vector2)Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()));
             var validTile = validZones.GetTile(tilePos);
 
             if(validTile != null)
@@ -73,7 +63,7 @@ public class ArmEditor : Draggable
                 transform.parent.parent.right = (validTileWorldPos - transform.parent.parent.position).normalized; 
                 transform.parent.position = validTileWorldPos;
                 transform.parent.position += Vector3.back;  //offset z by 2 so that the raycast on mouse relase may always hit this one first
-                SetArmLength(Mathf.RoundToInt(Vector3Int.Distance(tilePos, validZones.WorldToCell(transform.parent.parent.position))));
+                SetArmLength(Mathf.RoundToInt(Vector3Int.Distance(tilePos, validZones.WorldToCell((Vector2)transform.parent.parent.position))));
             }
             
             yield return null;
